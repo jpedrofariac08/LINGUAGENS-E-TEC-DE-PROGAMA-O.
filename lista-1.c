c4b7594e9aed64322e53f5628b201bc9e25b9451
João Pedro De Faria Corso <5wkpf97bhw@privaterelay.appleid.com>
2026-08-14 21:41:31 -0300

    Update lista-1.c

--- lista-1.c
+++ lista-1.c
@@ -11,7 +11,7 @@ int main(int argc, char *argv[]) {
 	scanf("%d", &primeiro);
 
 	printf("insira o segundo valor: \n");
-	scanf("%d \n", &segundo);
+	scanf("%d", &segundo);
 	
 	aux = primeiro;
 	primeiro = segundo;
@@ -19,35 +19,81 @@ int main(int argc, char *argv[]) {
 	
 	printf(" %d \n %d\n", primeiro, segundo);
 	
-	/* 2- Fa�a um programa que leia um valor do tipo double e depois imprima na forma de nota��o cientifica! */
+	/* 2- Faça um programa que leia um valor do tipo double e depois imprima na forma de notação cientifica! */
 	
 	double valor;
 	
 	printf("insira um valor: \n");
-	scanf("%lf \n", &valor);
+	scanf("%lf", &valor);
 	
 	printf("em notacao cientifica: %e \n", valor);
 	
 	/* 3- Implemente um programa que leia um numero n e mostre na tela o seu valor em base binaria(bits)! */
+    int n;
+int resto;
+int bits[10];
+int contador = 0;
+
+printf("Digite um numero: ");
+scanf("%d", &n);
+
+while (n > 0) {
+    resto = n % 2;
+    bits[contador] = resto;
+    contador++;
+
+    n = n / 2;
+}
+
+int i;
+
+printf("A conversao para binario resulta: ");
+
+for (i = 6; i >= 0; i--) {
+    if (i < contador)
+        printf("%d", bits[i]);
+    else
+        printf("0");
+}
+
+printf("\n");
 
 
-	
-	/* 5- Elabore um programa que pe�a ao usuario digitar 4 valores, e mostre na tela a soma, a media e o produtorio dos 4 valores! */
+printf("a conversao para binario resulta: ");
+for (i = contador - 1; i >= 0; i--) {
+    printf("%d", bits[i]);
+}
+printf("\n");
+    /* 4- Faça um programa que leia, o salário fixo e o valor total em vendas de um vendedor.
+    Sabendo que este vendedor ganha 15% de comissão sobre suas vendas efetuadas, mostre o total a
+    receber no final do mês, com duas casas decimais. */
+    
+    float salario, vendas, salarioTOTAL;
+    
+    printf("insira o valor do salario: \n");
+        scanf("%f", &salario);
+
+    printf("insira o valor total de vendas do vendedor: \n");
+        scanf("%f", &vendas);
+
+        salarioTOTAL = salario + (vendas * 0.15);
+        printf ("o salario total equivale: %f \n", salarioTOTAL);
+	/* 5- Elabore um programa que peça ao usuario digitar 4 valores, e mostre na tela a soma, a media e o produtorio dos 4 valores! */
 	
 	float num1, num2, num3, num4, soma, media, produtorio;
 
 	
-	printf("insira o primeiro valor: \n");
-	scanf("%f \n", &num1);
+	printf("\n insira o primeiro valor: \n");
+	scanf("%f", &num1);
 	
 	printf("insira o segundo valor: \n");
-	scanf("%f \n", &num2);
+	scanf("%f", &num2);
 	
 	printf("insira o terceiro valor: \n");
-	scanf("%f \n", &num3);
+	scanf("%f", &num3);
 	
 	printf("insira o quarto valor: \n");
-	scanf("%f \n", &num4);
+	scanf("%f", &num4);
 	
 	soma = num1 + num2 + num3 + num4;
 	media = ((num1 + num2 + num3 +num4)/4);
